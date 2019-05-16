a3f23e08f57c149544eec4d7311bbb27741929ee
Piotr Chorościn <titkot96@gmail.com>
2019-05-16 16:13:40 +0200

    First implementation of repository - only sail api, no cache

--- android_app/app/src/main/java/com/pwr/sailapp/data/network/sail/response/CentresResponse.kt
+++ android_app/app/src/main/java/com/pwr/sailapp/data/network/sail/response/CentresResponse.kt
@@ -3,6 +3,6 @@ package com.pwr.sailapp.data.network.sail.response
 import com.pwr.sailapp.data.Centre
 
 data class CentresResponse(
-    val centres: List<Centre>
+    val centres: ArrayList<Centre>
 ) {
 }
--- android_app/app/src/main/java/com/pwr/sailapp/data/repository/MainRepositoryImpl.kt
+++ android_app/app/src/main/java/com/pwr/sailapp/data/repository/MainRepositoryImpl.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import com.pwr.sailapp.data.Centre
 import com.pwr.sailapp.data.Equipment
 import com.pwr.sailapp.data.Rental
+import com.pwr.sailapp.data.network.sail.SailAppApiService
 import com.pwr.sailapp.data.network.sail.SailNetworkDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 // https://developer.android.com/jetpack/docs/guide
 
@@ -13,34 +16,48 @@ class MainRepositoryImpl(
     private val sailNetworkDataSource: SailNetworkDataSource
 ) : MainRepository {
 
+    private val centres = MutableLiveData<ArrayList<Centre>>()
+    private val allCentreGear = MutableLiveData<ArrayList<Equipment>>()
+    private val allUserRentals = MutableLiveData<ArrayList<Rental>>()
+
     init {
-        // observe forever (repos don't have lifecycle) changes in live data
+        // observe forever (repos don't have lifecycle) changes in live data (responses)
         sailNetworkDataSource.apply {
             downloadedCentres.observeForever {
-                // TODO ...
+                centres.postValue(it.centres)
             }
             downloadedAllCentreGear.observeForever {
-                // TODO ...
+                allCentreGear.postValue(it.gear)
             }
             downloadedAllUserRentals.observeForever {
-                // TODO ...
+                allUserRentals.postValue(it.rentals)
             }
         }
     }
 
     override suspend fun getAllUserRentals(userID: Int): LiveData<ArrayList<Rental>> {
-        sailNetworkDataSource.fetchAllUserRentals(userID)
-        //val result = sailNetworkDataSource.downloadedAllUserRentals
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return withContext(Dispatchers.IO) {
+            fetchAllUserRentals(userID)
+            allUserRentals
+        }
+
     }
 
     override suspend fun getAllCentreGear(centreID: Int): LiveData<ArrayList<Equipment>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return withContext(Dispatchers.IO) {
+            fetchAllCentreGear(centreID)
+            allCentreGear
+        }
     }
 
     override suspend fun getCentres(): LiveData<ArrayList<Centre>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return withContext(Dispatchers.IO) {
+            fetchCentres()
+            centres
+        }
     }
 
     private suspend fun fetchCentres(){ sailNetworkDataSource.fetchCentres() }
+    private suspend fun fetchAllCentreGear(centreID: Int){ sailNetworkDataSource.fetchAllCentreGear(centreID) }
+    private suspend fun fetchAllUserRentals(rentalID: Int){ sailNetworkDataSource.fetchAllUserRentals(rentalID) }
 }
