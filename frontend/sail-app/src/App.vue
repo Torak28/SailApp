90ef5f60e2521fce0d7723dda1bbc4015b13e75b
Torak28 <zelechowski28@gmail.com>
2019-05-18 17:17:27 +0200

    Add navbar! :clap:

--- frontend/sail-app/src/App.vue
+++ frontend/sail-app/src/App.vue
@@ -1,31 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <b-container fluid>
+      <b-navbar :sticky="true" type="light" class="nav-background">
+          <b-navbar-brand href="#" class="mx-auto">
+            <img src="./assets/logoNavbar.png" class="align-centre" alt="Logo">
+            Sail
+          </b-navbar-brand>
+        </b-navbar>
+      <div id="text">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        <router-view />
+      </div>
+    </b-container>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif !important;
+    background: #fafafa !important; 
+    color: #222;
+    margin-left: -15px;
+    margin-right: -15px;
+  }
+  #text {
+    color: #222;
+    padding: 2rem;
+    margin: 8px;
+    text-align: justify;
+    text-justify: inter-word;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  .nav-background {
+    background: #ece4b7;
+    min-height: 50px;
+  }
 </style>
