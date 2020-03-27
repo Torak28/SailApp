9de4e8277fe1ef1959f0330e904e1cdeac0290f5
Piotr Chorościn <titkot96@gmail.com>
2020-03-27 02:50:37 +0100

    Handle exception in ErrorHandlingInterceptor

--- android_app/app/src/main/java/com/pwr/sailapp/data/network/sail/ErrorHandlingInterceptor.kt
+++ android_app/app/src/main/java/com/pwr/sailapp/data/network/sail/ErrorHandlingInterceptor.kt
@@ -3,14 +3,19 @@ package com.pwr.sailapp.data.network.sail
 import com.pwr.sailapp.internal.ErrorCodeException
 import okhttp3.Interceptor
 import okhttp3.Response
+import kotlin.Exception
 
 class ErrorHandlingInterceptor : Interceptor{
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val response = chain.proceed(request)
-        if(response.isSuccessful) return response
-        else throw ErrorCodeException(response.code(), response.message())
+        try {
+            val response = chain.proceed(request)
+            if(response.isSuccessful) return response
+            else throw ErrorCodeException(response.code(), response.message())
+        } catch (e: Exception){
+            throw ErrorCodeException(-1, e.message?: "Unknown error occurred")
+        }
     }
 
 }
